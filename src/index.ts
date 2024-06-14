10f409d2ddca2672cf8bd498b61fd95f4aed0dcf
Luis Rodriguez <luisfrodriguezb.lf@gmail.com>
2024-06-14 19:40:36 -0400

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { Telegraf } from 'telegraf';
 
 import { about } from './commands';
-import { greeting } from './text';
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import { development, production } from './core';
 
@@ -11,7 +10,7 @@ const ENVIRONMENT = process.env.NODE_ENV || '';
 const bot = new Telegraf(BOT_TOKEN);
 
 bot.command('about', about());
-bot.on('message', greeting());
+bot.on('message', (ctx) => ctx.reply("Dorime"));
 
 //prod mode (Vercel)
 export const startVercel = async (req: VercelRequest, res: VercelResponse) => {
